23f798ef803ba275b18ede2ccc9d64c678b076ac
renukaatkare <atkarerenu@gmail.com>
2025-07-31 08:59:50 +0530

    Add files via upload

---
+++ conditional2.py
@@ -0,0 +1,15 @@
+age=21
+
+if (age >=18) :
+    print("can vote, & apply for license")
+
+
+    light = "red"
+
+    if(light == "red"):
+        print("stop")
+    elif(light == "green"):
+        print("go")
+    elif(light == "yellow"):
+        print("look")
+
