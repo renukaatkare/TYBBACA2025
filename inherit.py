c984fde80b656c0f54fca512f44ce8ecb7828ad7
renukaatkare <atkarerenu@gmail.com>
2025-08-12 23:37:40 +0530

    Add files via upload
    inheritance python

---
+++ inherit.py
@@ -0,0 +1,13 @@
+class person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+        
+    def printname(self):
+        print(self.firstname, self.lastname)
+            
+class student(person):
+    pass
+x = student("Ram", "Dhumal")
+x.printname()
+        
